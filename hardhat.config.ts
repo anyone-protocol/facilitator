98a51595de1abba2a326be43777e7ef989662a84
kanshi <46557+kanshi@users.noreply.github.com>
2023-07-22 05:56:24 +0200

    Config for Goerli network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
 import "@openzeppelin/hardhat-upgrades";
 
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY 
+  || "0x47c99abed3324a2707c28affff1267e45918ec8c3f20b8aa892e8b065d2942dd" # HardHat dev key no. 13
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.19",
@@ -10,6 +18,12 @@ const config: HardhatUserConfig = {
       optimizer: {
         enabled: true
       },
+    },
+  },
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [GOERLI_PRIVATE_KEY],
     },
   },
 };
