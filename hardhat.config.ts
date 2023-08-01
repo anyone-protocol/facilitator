5b8e1bd2cf776bf9b53f408e47fba0d1a73ae96e
kanshi <46557+kanshi@users.noreply.github.com>
2023-08-01 11:25:51 +0200

    Package fixes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,29 @@
-require('@nomicfoundation/hardhat-ethers')
+import { HardhatUserConfig } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY 
+  || "0x47c99abed3324a2707c28affff1267e45918ec8c3f20b8aa892e8b065d2942dd" // HardHat dev key no. 13
+
+const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.19",
     settings: {
       optimizer: {
-        enabled: true,
-        runs: 5
+        enabled: true
       },
     },
   },
   networks: {
-    hardhat: {
-      gas: 12000000,
-      blockGasLimit: 0x1fffffffffffff,
-      allowUnlimitedContractSize: true,
+    goerli: {
+      url: '',
+      // url: GOERLI_RPC_URL,
+      // accounts: [GOERLI_PRIVATE_KEY],
       timeout: 1800000
     },
-    goerli: {
-      url: "https://ethereum-goerli.publicnode.com",
-      accounts: [],
-    },
-  }
+  },
 };
+
+export default config;
