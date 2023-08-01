0a630d9c5f2239536349d69e8cbd0cee550b56f5
Jim Toth <jim@artby.city>
2023-07-31 23:18:34 -0400

    Fixes deploy scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+// import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
 import "@openzeppelin/hardhat-upgrades";
-
-import * as dotenv from "dotenv";
-
-dotenv.config();
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY 
@@ -24,6 +20,7 @@ const config: HardhatUserConfig = {
     goerli: {
       url: GOERLI_RPC_URL,
       accounts: [GOERLI_PRIVATE_KEY],
+      timeout: 1800000
     },
   },
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,32 @@
-import { ethers, upgrades } from "hardhat";
+import 'dotenv/config'
+import { ethers, upgrades } from 'hardhat'
 
 async function main() {
-  const ContractFactory = await ethers.getContractFactory("Facility");
+  const ATOR_TOKEN_CONTRACT_ADDRESS = process.env.ATOR_TOKEN_CONTRACT_ADDRESS || ''
+  const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY
+  const [ owner ] = await ethers.getSigners()
 
-  const instance = await upgrades.deployProxy(ContractFactory);
-  await instance.waitForDeployment();
+  const deployer = deployerPrivateKey
+    ? new ethers.Wallet(
+        deployerPrivateKey,
+        new ethers.JsonRpcProvider(process.env.JSON_RPC)
+      )
+    : owner
+  
+  console.log(`Deploying contract with deployer ${deployer.address}...`)
+  
+  const Contract = await ethers.getContractFactory('Facility', deployer)
+  
+  const instance = await upgrades.deployProxy(
+    Contract,
+    [ ATOR_TOKEN_CONTRACT_ADDRESS ]
+  )
+  await instance.waitForDeployment()
+  console.log(`Proxy deployed to ${await instance.getAddress()}`)
 
-  console.log(`Proxy deployed to ${await instance.getAddress()}`);
+  // const result = await Contract.deploy()
+  // await result.deployed()
+  // console.log(`Contract deployed to ${result.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
