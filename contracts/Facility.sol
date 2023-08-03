cb7255657039ced92b061f467ee12d040e1a3b6e
kanshi <46557+kanshi@users.noreply.github.com>
2023-08-03 16:29:53 +0200

    One step update and claim tokens, setters for GasCost/Price, var names cleanup

--- contracts/Facility.sol
+++ contracts/Facility.sol
@@ -15,8 +15,8 @@ contract Facility is Initializable, PausableUpgradeable, AccessControlUpgradeabl
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
-    address public tokenContract;
-    address payable public operator;
+    address public tokenAddress;
+    address payable public operatorAddress;
 
     uint256 public GAS_COST;
     uint256 public GAS_PRICE;
@@ -33,7 +33,7 @@ contract Facility is Initializable, PausableUpgradeable, AccessControlUpgradeabl
 
     receive() external payable whenNotPaused {
         availableBudget[msg.sender] += msg.value;
-        operator.transfer(msg.value);
+        operatorAddress.transfer(msg.value);
         emit GasBudgetUpdated(
             msg.sender,
             availableBudget[msg.sender] - usedBudget[msg.sender]
@@ -42,7 +42,7 @@ contract Facility is Initializable, PausableUpgradeable, AccessControlUpgradeabl
 
     function receiveAndRequestUpdate() external payable whenNotPaused {
         availableBudget[msg.sender] += msg.value;
-        operator.transfer(msg.value);
+        operatorAddress.transfer(msg.value);
         emit GasBudgetUpdated(
             msg.sender,
             availableBudget[msg.sender] - usedBudget[msg.sender]
@@ -54,17 +54,17 @@ contract Facility is Initializable, PausableUpgradeable, AccessControlUpgradeabl
         this._requestUpdate(msg.sender);
     }
 
-    function _requestUpdate(address addr) external whenNotPaused {
+    function _requestUpdate(address addr) public whenNotPaused {
         uint256 required = GAS_COST * GAS_PRICE;
-        uint256 _available = availableBudget[addr];
-        uint256 _used = usedBudget[addr];
+        uint256 available = availableBudget[addr];
+        uint256 used = usedBudget[addr];
 
         require(
-            _available > _used,
+            available > used,
             "Facility: requires user provided gas budget to create allocation updates"
         );
         require(
-            ( _available - _used ) >= required,
+            ( available - used ) >= required,
             "Facility: user provided budget is depleted, send ETH to contract address to refill"
         );
         
@@ -73,6 +73,14 @@ contract Facility is Initializable, PausableUpgradeable, AccessControlUpgradeabl
 
     function updateAllocation(address addr, uint256 _value) 
         external 
+        whenNotPaused 
+        onlyRole(OPERATOR_ROLE)
+    {
+        this._updateAllocation(addr, _value);
+    }
+
+    function _updateAllocation(address addr, uint256 _value) 
+        public 
         whenNotPaused 
         onlyRole(OPERATOR_ROLE)
     {
@@ -93,31 +101,47 @@ contract Facility is Initializable, PausableUpgradeable, AccessControlUpgradeabl
         emit AllocationUpdated(addr, _value);
     }
 
+    function updateAndClaimAllocation(address addr, uint256 _value)
+        external 
+        whenNotPaused 
+        onlyRole(OPERATOR_ROLE)
+    {
+        this._updateAllocation(addr, _value);
+        this._claimAllocation(addr);
+    }
+
     function claimAllocation()
         external 
         whenNotPaused
     {
+        this._claimAllocation(msg.sender);
+    }
+
+    function _claimAllocation(address addr)
+        external 
+        whenNotPaused
+    {
         require(
-            msg.sender != address(0),
+            addr != address(0),
             "Facility: can't claim allocation for 0x0"
         );
         
-        uint256 _available = allocatedTokens[msg.sender];
+        uint256 allocated = allocatedTokens[msg.sender];
         require(
-            _available > 0,
+            allocated > 0,
             "Facility: no tokens allocated for sender"
         );
 
         uint256 _claimed = claimedTokens[msg.sender];
         require(
-            _available > _claimed,
+            allocated > _claimed,
             "Facility: no tokens available to claim"
         );
 
-        IERC20 token = IERC20(tokenContract);
+        IERC20 token = IERC20(tokenAddress);
         uint256 contractBalance = token.balanceOf(address(this));
         
-        uint256 _claimable = _available - _claimed;
+        uint256 _claimable = allocated - _claimed;
         require(
             contractBalance > _claimable,
             "Facility: not enough tokens to claim"
@@ -128,8 +152,24 @@ contract Facility is Initializable, PausableUpgradeable, AccessControlUpgradeabl
             "Facility: transfer of claimable tokens failed"
         );
 
-        claimedTokens[msg.sender] = _available;
+        claimedTokens[msg.sender] = allocated;
         emit AllocationClaimed(msg.sender, _claimable);
+    }
+
+    function setGasCost(uint256 _value) 
+        external 
+        whenNotPaused 
+        onlyRole(OPERATOR_ROLE)
+    {
+        GAS_COST = _value;
+    }
+
+    function setGasPrice(uint256 _value) 
+        external 
+        whenNotPaused 
+        onlyRole(OPERATOR_ROLE)
+    {
+        GAS_PRICE = _value;
     }
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -138,11 +178,11 @@ contract Facility is Initializable, PausableUpgradeable, AccessControlUpgradeabl
     }
 
     function initialize(
-        address _tokenContract, 
+        address _tokenAddress, 
         address payable _operator
     ) initializer public {
-        tokenContract = _tokenContract;
-        operator = _operator;
+        tokenAddress = _tokenAddress;
+        operatorAddress = _operator;
         
         GAS_COST = 21_644;
         GAS_PRICE = 40 * 1_000_000_000;
@@ -154,7 +194,7 @@ contract Facility is Initializable, PausableUpgradeable, AccessControlUpgradeabl
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(UPGRADER_ROLE, msg.sender);
-        _grantRole(OPERATOR_ROLE, operator);
+        _grantRole(OPERATOR_ROLE, operatorAddress);
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
